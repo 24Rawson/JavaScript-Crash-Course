a53cefde6d92c52a437d190116b1eb6c421dee03
Paul <24rawson24@gmail.com>
2022-12-09 00:05:28 -0600

    Fizzbuzz Q commit

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-let count = 1;
-
-for (i = 0; i <= 20; ++i){
- if (i % 3 === 0) {
-    console.log('${i} -> Frontend')
- }
+for (let i=1; i <= 20; i++) {
+    console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Frontend Simplified')
+    }
+    else if (i % 3 === 0) {
+        console.log('Frontend')
+    }
+    else if (i % 5 === 0) {
+        console.log('Simplified')
+    }
 }
+
